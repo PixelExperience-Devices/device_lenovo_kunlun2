73f29a6e11b88e3c5ff75b36d0536f8d13ae9f36
Lucas Dupin <dupin@google.com>
2020-03-14 15:01:44 -0700

    kunlun2: Enable blurs, under dev-option
    Test: make
    Test: manual
    Bug: 149792636
    Change-Id: I607ce7de50545b87c3b4d98bb149355c84631e53
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -1,3 +1,9 @@
+# Blurs
+PRODUCT_PRODUCT_PROPERTIES += \
+   ro.surface_flinger.supports_background_blur=1 \
+   ro.sf.blurs_are_expensive=1 \
+   persist.sys.sf.disable_blurs=1
+
 # Charger
 PRODUCT_PRODUCT_PROPERTIES += \
    ro.charger.enable_suspend=true
