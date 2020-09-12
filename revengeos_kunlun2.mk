243fbf3ea878fdb321f01353039413a289795409
GiaSen <giammasena@gmail.com>
2020-09-12 17:10:15 +0200

    kunlun2: revengeos bringup

--- revengeos_kunlun2.mk
+++ revengeos_kunlun2.mk
@@ -16,16 +16,13 @@
 
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Pixel Experience targets
-TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/revengeos/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_kunlun2
+PRODUCT_NAME := revengeos_kunlun2
 PRODUCT_BRAND := lenovo
 PRODUCT_DEVICE := kunlun2
 PRODUCT_MANUFACTURER := lenovo
