3976e2254beda9ef25105bfe58aecf0057659be0
GiaSen <giammasena@gmail.com>
2020-11-08 11:29:24 +0100

    kunlun2: Nuke USB custom Hal

--- device.mk
+++ device.mk
@@ -144,10 +144,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/thermal-engine.conf
 
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.lenovo_kunlun2
-
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/wifi/WCNSS_qcom_cfg.ini
