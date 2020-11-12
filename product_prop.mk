77b3f1797abee124f88d7cffc642602c5d4fdd78
GiaSen <giammasena@gmail.com>
2020-11-13 00:10:11 +0100

    kunlun2: Update coral fp to nov

--- product_prop.mk
+++ product_prop.mk
@@ -23,7 +23,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Pixel identification
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.build.fingerprint=google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys
+    ro.build.fingerprint=google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys
 
 # Telephony
 PRODUCT_PRODUCT_PROPERTIES += \
