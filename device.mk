98dce4d4d50edd090869e5f9bd4e29e409f9a525
GiaSen <giammasena@gmail.com>
2021-01-01 17:53:10 +0100

    kunlun2: Nuke LenovoParts

--- device.mk
+++ device.mk
@@ -52,10 +52,6 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.power.rc
 
-# Device-specific settings
-PRODUCT_PACKAGES += \
-    LenovoParts
-
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \
