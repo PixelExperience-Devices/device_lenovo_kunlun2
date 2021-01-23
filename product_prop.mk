0e922201878ac41e23fdc6c9f1a6f76676d911a2
Giammarco Senatore <giammasena@gmail.com>
2021-01-23 12:23:38 +0100

    kunlun2: Enable ADB on eng builds

--- product_prop.mk
+++ product_prop.mk
@@ -1,3 +1,12 @@
+# Adb
+ifeq ($(TARGET_BUILD_VARIANT),eng)
+# /vendor/default.prop is force-setting ro.adb.secure=1
+# Get rid of that by overriding it on eng builds
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.secure=0 \
+    ro.adb.secure=0
+endif
+
 # Blurs
 PRODUCT_PRODUCT_PROPERTIES += \
    ro.surface_flinger.supports_background_blur=1 \
